616d5317e08e65e332c569d140e65e22aaa85d25
Sujin Kim <sujinkim919@gmail.com>
2023-10-10 21:51:53 +0900

    feat: add retrofit2 service enqueue code
    - for test !

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.network.api.PetApi
+import com.example.network.infrastructure.ApiClient
+import com.example.network.model.Pet
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pet = Pet(
+            "jiny",
+            listOf("https://get.pxhere.com/photo/nature-grass-flower-wildlife-kitten-cat-mammal-fauna-close-up-whiskers-vertebrate-domestic-cat-macro-photography-young-cat-red-mackerel-tabby-red-cat-cat-baby-wild-cat-small-to-medium-sized-cats-cat-like-mammal-634099.jpg"),
+        )
+        ApiClient().createService(PetApi::class.java).addPet(pet).enqueue(object : Callback<Pet> {
+            override fun onResponse(call: Call<Pet>, response: Response<Pet>) {
+                Log.d("ssuu", "onResponse: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<Pet>, t: Throwable) {
+                Log.d("ssuu", "onFailure: ${t.message}")
+            }
+        })
     }
 }
